23e2af9a06b4854b1a6dddeeef738f1d0d0bee51
Vivek Tyagi <tyagivivek435@gmail.com>
2025-07-20 13:31:40 +0530

    error navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
-// import Experiences from "./sections/Experiences";
+
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
 import Navbar from "./sections/navbar";
-// import Navbar from "./sections/navbar";
 
 const App = () => {
   return (
